0a5fdcd7ab4bcc78a8bc3e9b893936fa7c0d6585
yamsroun <yamsroun@gmail.com>
2025-02-24 00:23:12 +0900

    3.8 Reusable Cards

---
+++ lib/widget/currency-cart.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class CurrencyWidget extends StatelessWidget {
+  final String name, code, amount;
+  final IconData icon;
+  final bool isInverted;
+
+  final _whiteColor = Colors.white;
+  final _blackColor = const Color(0xFF1F2123);
+
+  const CurrencyWidget({
+    super.key,
+    required this.name,
+    required this.code,
+    required this.amount,
+    required this.icon,
+    required this.isInverted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: isInverted ? _whiteColor : _blackColor,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: isInverted ? _blackColor : _whiteColor,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color: isInverted ? _blackColor : _whiteColor,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      code,
+                      style: TextStyle(
+                        color: (isInverted ? _blackColor : _whiteColor)
+                            .withValues(alpha: 0.8),
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Transform.scale(
+              scale: 2.2,
+              child: Transform.translate(
+                offset: const Offset(-5, 12),
+                child: Icon(
+                  icon,
+                  color: isInverted ? _blackColor : Colors.white,
+                  size: 88,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
